0ffab49f2cda641facb468234d8f6c3557bec2c8
bitu364 <30683159+bitu364@users.noreply.github.com>
2017-08-07 23:28:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'profile1.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
 app.get('/article-two', function(req,res){
