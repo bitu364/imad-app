9b5a72b9dfbed940688251439b23a8846405ebff
bitu364 <30683159+bitu364@users.noreply.github.com>
2017-08-09 15:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var button= document.getElemenById('counter');
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    spab.innerHTML=counter.toString();
+};
