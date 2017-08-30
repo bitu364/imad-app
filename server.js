49e3f0b0ac6530732af774124455e75f68979edd
bitu364 <30683159+bitu364@users.noreply.github.com>
2017-08-30 22:36:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool = require('pg') Pool;
+var config = {
+    user: 'divit13',
+    database: 'duvmit13',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -30,6 +37,19 @@ app.get('/ui/photo.jpg', function (req, res) {
 
 app.get('/ui/1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+    // make a select request
+    // return a response with the results
+    pool.query('SELECT * FROM test', fuction(err,.result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 var counter =0;
 app.get('/counter',function(req,res){
