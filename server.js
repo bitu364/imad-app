0ad845eaaa8980e5d3161642185329f2b3dea250
bitu364 <30683159+bitu364@users.noreply.github.com>
2017-09-01 10:42:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user: 'divmit13',
     database: 'divmit13',
@@ -14,6 +15,14 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input, salt){
+       var hashed = crypto.pbkdf2Sync(input, salt,10000, 512, 'sha512');
+       return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input, 'this is some random string');
 });
 
 app.get('/profile', function(req,res){
